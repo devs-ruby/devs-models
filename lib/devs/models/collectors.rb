590e07f2d240ac24606ed18c684d405f77652dfd
Romain Franceschini <franceschini.romain@gmail.com>
2014-03-21 15:10:22 +0100

    Revert "added io logger"
    This reverts commit ea578deb47e73fdb11b052f273e24bedb95dcc46.

--- lib/devs/models/collectors.rb
+++ lib/devs/models/collectors.rb
@@ -1,4 +1,3 @@
 require 'devs/models/collectors/collector'
 require 'devs/models/collectors/csv_collector'
 require 'devs/models/collectors/plot_collector'
-require 'devs/models/collectors/io_logger'
