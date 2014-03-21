ea578deb47e73fdb11b052f273e24bedb95dcc46
Romain Franceschini <franceschini.romain@gmail.com>
2014-03-21 14:49:34 +0100

    added io logger

--- lib/devs/models/collectors.rb
+++ lib/devs/models/collectors.rb
@@ -1,3 +1,4 @@
 require 'devs/models/collectors/collector'
 require 'devs/models/collectors/csv_collector'
 require 'devs/models/collectors/plot_collector'
+require 'devs/models/collectors/io_logger'
