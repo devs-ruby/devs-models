3a238d981162cf752f050ed77062b3eeaeb9e825
Romain Franceschini <franceschini.romain@gmail.com>
2014-03-21 11:40:07 +0100

    fixed a bug in sinus generator

--- lib/devs/models/generators/sinus_generator.rb
+++ lib/devs/models/generators/sinus_generator.rb
@@ -2,24 +2,29 @@ module DEVS
   module Models
     module Generators
       class SinusGenerator < DEVS::AtomicModel
-        def initialize(amplitude=1.0, frequency=50.0, phase=0.0, step=20)
+        def initialize(amplitude=1.0, frequency=50.0, phase=0.0, step=20, qss_order = 2)
           super()
 
+          @qss_order = (1..3).include?(qss_order) ? qss_order : 2
           @amplitude = amplitude.to_f
           @frequency = frequency.to_f
           @phase = phase.to_f
           @step = step
           @pulsation = 2 * Math::PI * @frequency
-          @dt = 1 / @frequency / step.to_f
           @sigma = 0
         end
 
         def internal_transition
-          @sigma = @dt
+          @sigma = 1.0 / @frequency / @step
         end
 
         def output
-          value = @amplitude * @pulsation * Math.cos(@pulsation * (self.time + @sigma) * @phase)
+          value = case @qss_order
+          when 1 then @amplitude * Math.sin(@pulsation * (self.time + @sigma) + @phase)
+          when 2 then @amplitude * @pulsation * Math.cos(@pulsation * (self.time + @sigma) + @phase)
+          when 3 then -@amplitude * (@pulsation ** 2) * Math.sin(@pulsation * (self.time + @sigma) + @phase) / 2
+          end
+
           output_ports.each { |port| post(value, port) }
         end
 
